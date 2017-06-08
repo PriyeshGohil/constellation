95fd28cfd36b5fb0765972cecd651c8796ec58f3
Priyesh Gohil <priyesh114@hotmail.co.uk>
2017-06-09 00:48:49 +0100

    retrieving all blogs from firebase

--- src/components/AllBlogs.vue
+++ src/components/AllBlogs.vue
@@ -1,26 +1,47 @@
 <template>
   <section class='allBlogs container'>
-      <div class='blog'>
-      <figure class='blogPreview'>
-          <!--<img class='blogPreview__img' src='../assets/blogDummyImg.png'/>-->
-          <figcaption class="">
-            <ul class="blogPreview__hashtags">
-                <li>design</li>
-                <li>front-end</li>
-            </ul>
-            <h3 class="blogPreview__title">This might be the best Blog Ever</h3>
-          </figcaption>
-      </figure>
-      <div class="blogPreview__info">
-            <p><span>12 Augest 2017</span> <a href='#'>/ 1 Comment</a></p>
-          </div>
+      <div class='blog' v-for="blog in blogs">
+              <figure class='blogPreview'>
+                <figcaption class="">
+                  <ul v-for='hashtags in blog.hashtags' class="blogPreview__hashtags">
+                      <li>{{hashtags}}</li>
+                  </ul>
+                  <h3 class="blogPreview__title">{{blog.title}}</h3>
+                </figcaption>
+              </figure>
 
-          </div>
+              <div class="blogPreview__info">
+                <p><span>{{blog.postedDate}}</span> <a href='#'>/ 1 Comment</a></p>
+              </div>
+      </div>
+
+
+      
+            <!--{{blogs}}-->
   </section>
+
 </template>
 
 <script>
+import {db} from '../firebase.js'
+
 export default {
+  data() {
+    return {
+      blogs: {}
+    }
+  },
+
+  methods: {
+    getBlogs() {
+      db.ref('blog').on('value', snap => this.blogs = snap.val());
+    },
+  },
+
+  created() {
+    console.log('yo!');
+    this.getBlogs();
+  }
 }
 </script>
 
