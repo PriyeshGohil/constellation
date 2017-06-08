46305d50981ab8210ad6b3f81cd5c2b24ff49bbd
Priyesh Gohil <priyesh114@hotmail.co.uk>
2017-06-08 01:07:51 +0100

    created blogPreview component and applied basic styling

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,16 @@
       <h1 class='logo'>constellation</h1>
       <p class='introTxt'><i>Donec rutrum congue leo eget malesuada. Vestibulum ante ipsum primis in faucibus orci luctus</i></p>
     </div>
+
+    <allBlogs></allBlogs>
   </div>
 </template>
 
 <script>
+import AllBlogs from './components/AllBlogs.vue'
 export default {
+
+  components: {'allBlogs': AllBlogs},
   name: 'app',
   data () {
     return {
---
+++ src/components/AllBlogs.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class='allBlogs container'>
+      <div class='blog'>
+      <figure class='blogPreview'>
+          <!--<img class='blogPreview__img' src='../assets/blogDummyImg.png'/>-->
+          <figcaption class="">
+            <ul class="blogPreview__hashtags">
+                <li>design</li>
+                <li>front-end</li>
+            </ul>
+            <h3 class="blogPreview__title">This might be the best Blog Ever</h3>
+          </figcaption>
+      </figure>
+      <div class="blogPreview__info">
+            <p><span>12 Augest 2017</span> <a href='#'>/ 1 Comment</a></p>
+          </div>
+
+          </div>
+  </section>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+
+</style>
