6d9ca7ccd4a13c5665daadd368ef6861a571145c
Priyesh Gohil <priyesh114@hotmail.co.uk>
2017-06-09 01:50:39 +0100

    displaying a single blog form the firbase db

--- src/components/SingleBlog.vue
+++ src/components/SingleBlog.vue
@@ -1,32 +1,64 @@
 <template>
-    <div class="blog">
-        <h2 class='blog__title'>Yo!</h2>
-        <img src=""/>
+    <div class="container blogPost">
+        <!--{{blog}}-->
+        <h2 class='blogPost__title'>{{blog.title}}</h2>
+        <img class="blogPost__img" src="https://images.pexels.com/photos/192632/pexels-photo-192632.jpeg?w=940&h=650&auto=compress&cs=tinysrgb"/>
         
-        <ul class='blog__hashtags'>
-            <li>web</li>
-            <li>programming</li>
+        <ul class='blogPreview__hashtags'>
+            <li v-for='hashtag in blog.hashtags'>{{hashtag}}</li>
         </ul>
         
-        <p class="blog__content">blog content</p>
+        <p class="blogPost__content">{{blog.content}}</p>
         
-        <div class='blog__nav'>
+        <div class='blogPost__nav'>
             <a href='#'>Preview</a>
             <a href='#'>Next</a>
         </div>
-
-        <div class='blog__embed'>
+        <div>
+            <p>{{blog.postedDate}} / comments 2</p>
+        </div>
+        <div class='blogPost__embed'>
             <pre>add embed code</pre>
         </div>
     </div>
 </template>
 
 <script>
-export default {
+import {db} from '../firebase.js'
+
+export default { 
+    data() {
+        return {
+            blog: {},
+            blogId :this.$route.params.id
+        }
+    },
+
+    methods: {
+        getBlog() {
+            const blogRef = db.ref('blogs/' + this.blogId);
+            blogRef.once('value', snapshot => this.blog = snapshot.val());
+        }
+    },
+
+    created() {
+        this.getBlog();
+    }
 
 }
 </script>
 
 <style>
+    .blogPost__title {
+       text-align: center;
+    }
+
+    .blogPost__img {
+        display: block;
+        margin: 0 auto;
+        width: 100%;
+        height: 300px;
+    }
+
 
 </style>
