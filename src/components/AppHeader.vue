aecc64628984fef8d8ee2903e1af0474078fa06a
Priyesh Gohil <priyesh114@hotmail.co.uk>
2017-06-15 03:15:39 +0100

    styling search box, you can not really perform fuzzy matching in firebase so will come back to this feature later

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
     <header class="header">
-      <div v-on:click='toggleMenu' class="header__menuIcon"><i class="fa fa-bars" aria-hidden="true"></i></div>
-      <div class="header__search"><i class="fa fa-search" aria-hidden="true"></i>search...</div>
+      <div v-on:click='toggle("showMenu")' class="header__menuIcon"><i class="fa fa-bars" aria-hidden="true"></i></div>
+      <div v-on:click='toggle("showSearch")' class="header__search"><i class="fa fa-search" aria-hidden="true"></i>search...</div>
     </header>
     
     <transition name='slide-menu'>
       <div  v-show="showMenu" class='menu-wrapper'>
-        <div class="menu__exit"><i v-on:click='toggleMenu' class="fa fa-times" aria-hidden="true"></i></div>
+        <div class="menu__exit"><i v-on:click='toggle("showMenu")' class="fa fa-times" aria-hidden="true"></i></div>
         <div class="menuSection">
           <h2 class="menuSection__heading">Hello.</h2>
           <p class="menuSection__txt">Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Donec rutrum congue leo eget malesuada. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.</p>
@@ -29,6 +29,22 @@
         </nav>
       </div>
     </transition>
+    <!--maybe I should move this out into its own component-->
+    <div v-show="showSearch" class="search-wrapper">
+      <div class="search__close"><i v-on:click='toggle("showSearch")' class="fa fa-times" aria-hidden="true"></i></div>
+      
+      <div class="search">
+        <div class="search__input">
+          <i class="fa fa-search" aria-hidden="true"></i>
+          <input type='text' placeholder="Type something..."/>
+        </div>
+        <ul class="search__results">
+          <li><a href='#'>Blog 1</a></li>
+          <li><a href='#'>Blog 2</a></li>
+        </ul>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -36,13 +52,14 @@
 export default {
   data() {
       return {
-        showMenu: false
+        showMenu: false,
+        showSearch: false
       }
   },
 
   methods: {
-    toggleMenu() {
-      this.showMenu = !this.showMenu;
+    toggle(elName) {
+      this[elName] = !this[elName];
     }
   }
 }
