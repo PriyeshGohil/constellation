0acaf9806820fbc0d099ec5441ff85ef435cbd02
Priyesh Gohil <priyesh114@hotmail.co.uk>
2017-06-20 18:51:40 +0100

    passing the current logged in user id to child components

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="app"> 
-    <Appheader></Appheader>
+    <Appheader v-bind:userId="userId"></Appheader>
     <div class='container'>
       <h1 class='logo'>constellation</h1>
       <p class='introTxt'><i>Donec rutrum congue leo eget malesuada. Vestibulum ante ipsum primis in faucibus orci luctus</i></p>
     </div>
-    <router-view></router-view>
+    <router-view v-bind:userId='userId'></router-view>
   </div>
 </template>
 
@@ -13,14 +13,29 @@
 import AllBlogs from './components/AllBlogs.vue'
 import AddBlog from './components/AddBlog.vue'
 import AppHeader from './components/AppHeader.vue'
-export default {
+import {auth} from './firebase.js'
 
+export default {
   components: {'allBlogs': AllBlogs, 'addBlog': AddBlog, 'Appheader' : AppHeader},
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      userId : null
     }
+  },
+
+  created() {
+    // check if a user is signed in.
+    auth.onAuthStateChanged(user => {
+      if(user) {
+        console.log('hey user: ', user.uid);
+        this.userId = user.uid;
+      }else {
+        this.userId = null;
+        console.log('not user');
+      }
+    })
   }
 }
 </script>
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import AddBlog from './components/AddBlog.vue'
 import AllBlogs from './components/AllBlogs.vue'
 import SingleBlog from './components/SingleBlog.vue'
+import SignUp from'./components/SignUp.vue'
 
 export default [
     {path: '/', component: AllBlogs},
-    {path: '/addBlog', component: AddBlog},
-    {path: '/blog/:id', component: SingleBlog}
+    {path: '/addBlog', component: AddBlog, props: true},
+    {path: '/blog/:id', component: SingleBlog},
+    {path: '/signUp', component: SignUp}
 
 ]
